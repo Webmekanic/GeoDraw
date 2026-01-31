4590cb4b0c4553277a55ac8e5facbfab6be6a742
webmekanic <aniebovictor001@gmail.com>
2026-01-31 18:32:12 +0100

    Feat: Setup leaflet

---
+++ index.js
@@ -0,0 +1,7 @@
+// create a map    
+const map = L.map('map').setView([-1.286389, 36.817223], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
