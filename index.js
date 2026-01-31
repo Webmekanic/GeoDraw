07135ca9375858a181f001629c47a5cf7ab3bcef
webmekanic <aniebovictor001@gmail.com>
2026-01-31 18:38:23 +0100

    Feat: Add line string

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // create a map    
-const map = L.map('map').setView([-1.286389, 36.817223], 13);
+const map = L.map("map").setView([-1.286389, 36.817223], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// feature group to store draw items
+
+const drawItems = new L.FeatureGroup();
+map.addLayer(drawItems);
+
+// draw controls
+
+const drawControl = new L.Control.Draw({
+  edit: { featureGroup: drawItems },
+  draw: {
+    // circle: false,
+    rectangle: false,
+    marker: false,
+    circlemarker: false,
+  },
+});
+
+map.addControl(drawControl);
+
+// handle draw events
+map.on(L.Draw.Event.CREATED, function (e) {
+  const layer = e.layer;
+  drawItems.addLayer(layer);
+
+  const geojson = layer.toGeoJSON();
+  let result = "";
+
+  if (geojson.geometry.type === "LineString") {
+    const length = turf.length(geojson, { units: "kilometers" });
+    result = `Distance: ${length.toFixed(2)} km`;
+  }
+  layer.bindPopup(result).openPopup();
+});
